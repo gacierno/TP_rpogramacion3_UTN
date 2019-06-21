f336c1eb2253a7e0867257caf5504d5cb2f8821b
gacierno <gacierno@gmail.com>
2019-06-21 18:22:27 -0300

    Class Domicilio

---
+++ src/Domicilio.java
@@ -0,0 +1,141 @@
+public class Domicilio {
+    /*
+    *   -Calle
+        -Numero
+        -CodigoPostal
+        -Ciudad
+        -Provincia
+        -Pais
+
+    * */
+
+    private String calle;
+    private int numero;
+    private String piso;
+    private String departamento;
+    private int codigoPostal;
+    private String ciudad;
+    private String provincia;
+    private String pais;
+
+    /*
+    *
+    *   CONSTRUCTORS
+    *
+    * */
+
+    //  CREAR UNA DIRECCION VACIA PARA CREAR RAPIDAMENTE CONTRATOS SIN TENER QUE CARGAR DATOS
+    public Domicilio() {
+        this.setCalle("");
+        this.setNumero(0);
+        this.setPais("");
+        this.setDepartamento("");
+        this.setCodigoPostal(0);
+        this.setCiudad("");
+        this.setProvincia("");
+        this.setProvincia("");
+    }
+
+    public Domicilio(String calle, int numero, String piso, String departamento, int codigoPostal, String ciudad, String provincia, String pais) {
+        this.setCalle(calle);
+        this.setNumero(numero);
+        this.setPais(piso);
+        this.setDepartamento(departamento);
+        this.setCodigoPostal(codigoPostal);
+        this.setCiudad(ciudad);
+        this.setProvincia(provincia);
+        this.setProvincia(pais);
+    }
+
+
+    /*
+     *
+     *   SETTERS
+     *
+     * */
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setPiso(String piso) {
+        this.piso = piso;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /*
+     *
+     *   GETTERS
+     *
+     * */
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getPiso() {
+        return piso;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    /*
+     *
+     *   TOSTRING
+     *
+     * */
+
+    @Override
+    public String toString() {
+        String output = String.format(
+                "Domicilio : {\n\tCale : %s,\n\tNumero : %d\n}",
+                this.getCalle(), this.getNumero()
+        );
+        return output;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Domicilio output = new Domicilio();
+        System.out.println( output );
     }
 }
