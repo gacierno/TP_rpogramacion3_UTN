72e7ff99d6f183a2c91e68e92eef6bc822ab1fd9
gacierno <gacierno@gmail.com>
2019-06-30 20:08:53 -0300

    Main Frame 1

---
+++ src/VistaMain.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+
+public class VistaMain {
+
+    private JFrame mainFrame;
+
+    /*
+    *   CONSTRUCTOR
+    * */
+
+    /*
+    *   SETTERS
+    * */
+
+    public void setMainFrame(){
+        this.mainFrame = new JFrame();
+    }
+
+    public void setButtonCrearAquiler(){
+
+    }
+
+    public void setButtonAgregarCliente(){
+
+    }
+
+    public void setButtonModificarCliente(){
+
+    }
+
+    public void setButtonBorarCliente(){
+        
+    }
+
+    /*
+    *   GETTERS
+    * */
+
+
+
+}
